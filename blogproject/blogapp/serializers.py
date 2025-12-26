1947d550f1cbdd64f203cac9ba9192656d654273
Carepred <carepred.ballos88@gmail.com>
2025-12-26 19:39:03 +1200

    create reaction buttons in DRF and Vue

--- blogproject/blogapp/serializers.py
+++ blogproject/blogapp/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Post, Profile
+from .models import Post, Profile, Reaction
 
 class Postserializers(serializers.ModelSerializer):
     class Meta:
@@ -11,10 +11,15 @@ class Postserializers(serializers.ModelSerializer):
         read_only_fields = ['author','created_at'] #These fields are not required for input
 
 
-#serializer for Profile page
+#serializer for Profile model
 class Profileserializers(serializers.ModelSerializer):
     class Meta:
         model = Profile
         # WE can keep it like this, still working. Soon need authentication for the current user
         fields = ['profile_id','bio','profile_picture','user']
 
+#serializer for Reaction model
+class Reactionserializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reaction
+        fields = ['reaction_id','post', 'reaction']
